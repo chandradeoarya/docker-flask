6239b92b690a1e8957112748edb9ed5fe0fb39e8
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-07-28 12:51:35 +0530

    stoppping build for demo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Docker compose github registry CI without tag!'
+  return 'Hello, Docker compose!'
 
 @app.route('/widgets')
 def get_widgets() :
