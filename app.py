1525c2f2abef2bc2f8ba3bf4c0062294b9464892
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-07-28 08:47:06 +0530

    adding CI with tag app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Docker compose CI1!'
+  return 'Hello, Docker compose CI with tag!'
 
 @app.route('/widgets')
 def get_widgets() :
