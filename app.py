c2a7f62a7aa697908067c3abe49297b2f20ef9b1
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-07-28 13:14:51 +0530

    adding workflow2 for demo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Docker compose!'
+  return 'Hello, Docker compose using github actions CI!'
 
 @app.route('/widgets')
 def get_widgets() :
